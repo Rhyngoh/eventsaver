36b85c2cc78844ed11cf4d6c3bfd3593d8bc6cdc
Ryan Ngoh <rxn120230@utdallas.edu>
2016-12-09 14:02:39 -0600

    modified schema

--- schema.sql
+++ schema.sql
@@ -1,26 +1,13 @@
-/*
-
-To run this file, we do the following in our Terminal:
-
-1. Go to the directory of this sql file.
-
-2. Get into our mysql console.
-
-3. Run "source schema.sql"
-
-*/
-
--- Create the database day_planner_db and specified it for use.
-CREATE DATABASE day_planner_db;
-USE day_planner_db;
-
--- Create the table plans.
-CREATE TABLE plans
+CREATE TABLE events
 (
 id int NOT NULL AUTO_INCREMENT,
-plan varchar(255) NOT NULL,
+event varchar(255) NOT NULL,
+createdat TIMESTAMP NOT NULL,
 PRIMARY KEY (id)
 );
 
 -- Insert a set of records.
-INSERT INTO plans (plan) VALUES ('Plan to fight a ninja.');
+INSERT INTO events (event) VALUES ('Today I drew a picture of my friends and I buying kale.');
+INSERT INTO events (event) VALUES ('Shaan made a song.');
+INSERT INTO events (event) VALUES ('Davon is rich. He is flipping houses.');
+SELECT * FROM events;
