aa33f03fa28031783635554da4ece140e8d1d80a
Ryan Ngoh <rxn120230@utdallas.edu>
2016-12-09 14:18:29 -0600

    modified server js

--- schema.sql
+++ schema.sql
@@ -1,13 +1,11 @@
-CREATE TABLE events
+-- Create the table plans.
+CREATE TABLE plans
 (
 id int NOT NULL AUTO_INCREMENT,
-event varchar(255) NOT NULL,
+plan varchar(255) NOT NULL,
 createdat TIMESTAMP NOT NULL,
 PRIMARY KEY (id)
 );
 
 -- Insert a set of records.
-INSERT INTO events (event) VALUES ('Today I drew a picture of my friends and I buying kale.');
-INSERT INTO events (event) VALUES ('Shaan made a song.');
-INSERT INTO events (event) VALUES ('Davon is rich. He is flipping houses.');
-SELECT * FROM events;
+INSERT INTO plans (plan) VALUES ('Plan to fight a ninja.');
